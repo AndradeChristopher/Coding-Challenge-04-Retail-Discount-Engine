d6f55127f62e935b2fe2c8cb5cab52ceca04607f
Christopher Andrade <christopherandrade@usf.edu>
2026-06-06 15:54:45 -0400

    Update cc_4.js
    Added listings.

--- js/cc_4.js
+++ js/cc_4.js
@@ -1,1 +1,32 @@
-
+let products = [
+    {
+        name: "Laptop",
+        category: "electronics",
+        price: 1000,
+        inventory: 10
+    },
+    {
+        name: "T-Shirt",
+        category: "apparel",
+        price: 25,
+        inventory: 20
+    },
+    {
+        name: "Milk",
+        category: "groceries",
+        price: 5,
+        inventory: 30
+    },
+    {
+        name: "Detergent",
+        category: "household",
+        price: 12,
+        inventory: 15
+    },
+    {
+        name: "Book",
+        category: "other",
+        price: 20,
+        inventory: 8
+    }
+];

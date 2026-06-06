affc7f97f3268a1fa0a10d4995a55184acaead97
Christopher Andrade <christopherandrade@usf.edu>
2026-06-06 15:56:46 -0400

    Update cc_4.js
    Discount per category and customer type discount.

--- js/cc_4.js
+++ js/cc_4.js
@@ -30,3 +30,39 @@ let products = [
         inventory: 8
     }
 ];
+
+for (const product of products) {
+
+    switch (product.category) {
+
+        case "electronics":
+            product.price *= 0.80; //20% off
+            break;
+
+        case "apparel":
+            product.price *= 0.85; //15% off
+            break;
+
+        case "groceries":
+        case "household":
+            product.price *= 0.90; //10% off
+            break;
+
+        default:
+            //0% discount
+            break;
+    }
+}
+
+let customerType = "student";
+let extraDiscount = 0;
+
+if (customerType === "student") {
+    extraDiscount = 0.05;
+}
+else if (customerType === "senior") {
+    extraDiscount = 0.07;
+}
+else {
+    extraDiscount = 0; //no/invalid selection
+}
